8c1fd1cd1c85a1dff62ef1b7f5394f69a4499845
mrmcplad <mrmcplad@gmail.com>
2021-06-10 13:01:36 -0600

    Update widget.service.ts

--- services/widget.service.ts
+++ services/widget.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
-import { Widget } from "@sf/submitter/components/widget-display/widget.interface";
+import { Widget } from "../widget.interface";
 
 @Injectable()
 export class WidgetService {
     private _widgetCount: number = 0;
+    
+    constructor() {}
 
     getWidgetsFromDatabase(): Observable<Widget[]> {
         // simulated data retrieval
         return of([
             {
-                iconURL: "/sf/ui/images/icons/widget-alpha.png",
+                id: 1,
+                iconURL: "/icons/widget-alpha.png",
                 tooltip: "Widget Alpha",
                 name: "Alpha",
                 description: "Alpha is the funnest widget",
                 releaseDate: new Date("2021/06/10")
             },
             {
-                iconURL: "/sf/ui/images/icons/widget-gamma.png",
+                id: 2,
+                iconURL: "/icons/widget-gamma.png",
                 tooltip: "Widget Gamma",
                 name: "Gamma",
                 description: "Gamma is the weirdest widget",
                 releaseDate: new Date("2021/06/08")
             },
             {
-                iconURL: "/sf/ui/images/icons/widget-omega.png",
+                id: 3,
+                iconURL: "/icons/widget-omega.png",
                 tooltip: "Widget Omega",
                 name: "Omega",
                 description: "Omega is the angriest widget",
